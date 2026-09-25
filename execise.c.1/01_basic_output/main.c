bce3ea97de9b7b318a8e6df6b5e859adea5c463d
jowinz-dev <your-github-jowinflux@gmail.com>
2026-09-25 22:34:33 +0300

    Create main.c

---
+++ execise.c.1/01_basic_output/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("==================================================\n");
+    printf("         STRUCTURED PROGRAMMING PRACTICE          \n");
+    printf("==================================================\n");
+    printf(" Student Name       :  Owinjo Julius Moses\n");
+    printf(" Student Access No  :  Bachelors of Science in Computer Science\n");
+    printf(" Course Name        :  Bachelors of Science in Computer Science\n");
+    printf(" Institution        :  Uganda Christian University (UCU)\n");
+    printf("--------------------------------------------------\n");
+    printf(" Assignment         :  Required Programming Categories\n");
+    printf(" Task No.           :  01\n");
+    printf(" Category           :  Basic Output - Demonstration of printf()\n");
+    printf(" Status             :  Successfully Configured & Linked to GitHub\n");
+    printf("==================================================\n");
+
+    return 0;
+}
