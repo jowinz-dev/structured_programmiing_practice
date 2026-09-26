3e427435fad084008a3a0dfe16b8d04252032dbe
jowinz-dev <your-github-jowinflux@gmail.com>
2026-09-26 17:29:19 +0300

    editted some parts

--- execise.c.1/01_basic_output/main.c
+++ execise.c.1/01_basic_output/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-    printf("==================================================\n");
+    printf("===================================================================\n");
     printf("         STRUCTURED PROGRAMMING PRACTICE          \n");
-    printf("==================================================\n");
+    printf("===================================================================\n");
     printf(" Student Name       :  Owinjo Julius Moses\n");
-    printf(" Student Access No  :  Bachelors of Science in Computer Science\n");
+    printf(" Student Access No  :  B39097\n");
     printf(" Course Name        :  Bachelors of Science in Computer Science\n");
     printf(" Institution        :  Uganda Christian University (UCU)\n");
-    printf("--------------------------------------------------\n");
+    printf("--------------------------------------------------------------------\n");
     printf(" Assignment         :  Required Programming Categories\n");
     printf(" Task No.           :  01\n");
     printf(" Category           :  Basic Output - Demonstration of printf()\n");
     printf(" Status             :  Successfully Configured & Linked to GitHub\n");
-    printf("==================================================\n");
+    printf("=====================================================================\n");
 
     return 0;
 }
